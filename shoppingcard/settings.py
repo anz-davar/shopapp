fbe418c2f508f0cbb3200cfe255ff2ba8ce4fceb
anz-davar <anzori94@gmail.com>
2025-01-30 22:01:46 +0200

    new db

--- shoppingcard/settings.py
+++ shoppingcard/settings.py
@@ -83,7 +83,8 @@ WSGI_APPLICATION = 'shoppingcard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_new.sqlite3',
     }
 }
 
